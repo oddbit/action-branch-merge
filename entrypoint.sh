3261d37e43807c9b20cc4d487da611ea45211ece
Dennis Alund <dennis.alund@gmail.com>
2020-05-05 22:15:34 +0800

    Pulling origin

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,12 +49,10 @@ git config --global user.email "$INPUT_USER_EMAIL"
 
 set -o xtrace
 
-git checkout -B $INPUT_TARGET_BRANCH
-git branch --set-upstream-to=origin/$INPUT_TARGET_BRANCH $INPUT_TARGET_BRANCH 
-git pull
 git checkout -B $INPUT_SOURCE_BRANCH
-git branch --set-upstream-to=origin/$INPUT_SOURCE_BRANCH $INPUT_SOURCE_BRANCH
-git pull
+git pull origin $INPUT_SOURCE_BRANCH
+git checkout -B $INPUT_TARGET_BRANCH
+git pull origin $INPUT_TARGET_BRANCH
 
 if git merge-base --is-ancestor $INPUT_SOURCE_BRANCH $INPUT_TARGET_BRANCH; then
   echo "No merge is necessary"
